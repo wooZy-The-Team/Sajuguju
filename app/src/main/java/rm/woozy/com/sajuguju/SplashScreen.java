dacdd019cbb9ede7fc53b4ed7b2442a645ae261d
Abdur Rahim <rahim.prsf@gmail.com>
2015-11-03 11:50:25 +0600

    Uploading

---
+++ app/src/main/java/rm/woozy/com/sajuguju/SplashScreen.java
@@ -0,0 +1,28 @@
+package rm.woozy.com.sajuguju;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(SplashScreen.this , LoginScreen.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
