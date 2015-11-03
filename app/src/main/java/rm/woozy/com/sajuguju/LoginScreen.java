57d8035afe28d5ca68453d6d351413a79d85279d
Moon <me.moon29@gmail.com>
2015-11-03 14:36:30 +0600

    Commit by moon

--- app/src/main/java/rm/woozy/com/sajuguju/LoginScreen.java
+++ app/src/main/java/rm/woozy/com/sajuguju/LoginScreen.java
@@ -9,5 +9,7 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        // Comment by moon
     }
 }
