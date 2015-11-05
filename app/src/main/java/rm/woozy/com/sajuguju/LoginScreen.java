691c59242ef10a0c1ea50f414e9e0beb58b79c7b
Moon <me.moon29@gmail.com>
2015-11-05 08:12:52 +0600

    Commit by moon
    nav_drawer & fab

--- app/src/main/java/rm/woozy/com/sajuguju/LoginScreen.java
+++ app/src/main/java/rm/woozy/com/sajuguju/LoginScreen.java
@@ -2,6 +2,8 @@ package rm.woozy.com.sajuguju;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class LoginScreen extends AppCompatActivity {
         setContentView(R.layout.activity_login_screen);
 
         // Comment by moon
+    }
+    public void signIn(View view){
+
+
     }
 }
