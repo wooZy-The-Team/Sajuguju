9f0a86df9f57d8a0d8f8ca528436bb7991fa8fd9
Moon <me.moon29@gmail.com>
2015-11-21 20:18:15 +0600

    AddRequest Activity
    -Moon

---
+++ app/src/main/java/rm/woozy/com/sajuguju/Preference/AddRequest.java
@@ -0,0 +1,15 @@
+package rm.woozy.com.sajuguju.Preference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rm.woozy.com.sajuguju.R;
+
+public class AddRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_request);
+    }
+}
